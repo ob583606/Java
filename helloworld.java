bd894b064eabb29496dd650653e6d06b7b0fedd6
ob583606 <50464245+ob583606@users.noreply.github.com>
2022-01-18 13:24:41 -0800

    First Program

---
+++ helloworld.java
@@ -0,0 +1,12 @@
+public class helloworld {
+
+    public static void printGreeting() {
+        System.out.println("Fuck this syntax");
+    }
+
+    public static void main(String[] args) {
+        printGreeting();
+        System.out.println("Hello World!");
+
+    }
+}
